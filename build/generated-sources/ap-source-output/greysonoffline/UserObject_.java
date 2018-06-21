022453c8d00b613d5a45e7930c00a6697ca70d89
davidsery <32872037+davidsery@users.noreply.github.com>
2018-06-22 00:40:49 +0200

    Add files via upload
    oprava naposledy sa nenahrala zlozka dist spravne

---
+++ build/generated-sources/ap-source-output/greysonoffline/UserObject_.java
@@ -0,0 +1,15 @@
+package greysonoffline;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-22T00:34:57")
+@StaticMetamodel(UserObject.class)
+public class UserObject_ { 
+
+    public static volatile SingularAttribute<UserObject, Integer> score;
+    public static volatile SingularAttribute<UserObject, String> name;
+    public static volatile SingularAttribute<UserObject, Long> id;
+
+}
